8974fad0d757d6cd53576c71adafe09b2f555f34
deliarusu <delia.rusu@gmail.com>
2017-01-16 17:15:58 +1300

    hatebase api read

---
+++ hatebase.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import sys
+
+try:
+    r = requests.get('http://api.hatebase.org/v3-0/xxx...x/vocabulary/json/a'
+                     'bout_language%3D1%7Clanguage%3D1')
+except requests.exceptions.RequestException as e:
+    print(e)
+    sys.exit(1)
+
+try:
+    d = json.loads(r.text)
+    data = d.get('data')
+    print(data)
+    datapoints = data.get('datapoint')
+    for dpts in datapoints:
+        print(dpts)
+        print(dpts.get('language'), dpts.get('meaning'))
+
+except ValueError as e:
+    print(e)
+    sys.exit(1)
