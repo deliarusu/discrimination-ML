aba5b1342e8230054f86a646ada794f59b278d23
deliarusu <delia.rusu@gmail.com>
2017-01-16 17:21:07 +1300

    added comments to hatebase.py

--- hatebase.py
+++ hatebase.py
@@ -2,13 +2,16 @@ import requests
 import json
 import sys
 
+# make request to hatebase, note the paging
 try:
+    # fill in API Key
     r = requests.get('http://api.hatebase.org/v3-0/xxx...x/vocabulary/json/a'
                      'bout_language%3D1%7Clanguage%3D1')
 except requests.exceptions.RequestException as e:
     print(e)
     sys.exit(1)
 
+# read json request output
 try:
     d = json.loads(r.text)
     data = d.get('data')
